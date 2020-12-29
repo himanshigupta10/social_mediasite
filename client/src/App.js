8cb24b65311ca8ef9229f8ee2d83eb619d4081e3
himanshigupta10 <himanshigupta600@gmail.com>
2020-12-30 00:07:26 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    // eslint-disable-next-line no-undef
+
+    <div><h1>Hi world
+      </h1></div>
+
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => ({ req, pubsub })
+});
+
+mongoose
+  .connect(MONGODB, { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+     })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5005 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
+  
